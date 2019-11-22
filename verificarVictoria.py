404e0746b6ddc9141e003cf9971c8b26907d3030
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2019-11-22 16:47:49 +0100

    subo última versión del programa, ya funcionan los turnos

--- verificarVictoria.py
+++ verificarVictoria.py
@@ -2,6 +2,7 @@ import random
 import time
 
 def mostrarMenu():
+    """Isma creará una función que sea mostrarInstrucciones con return instrucciones y enviar a esta función"""
     print("*****************************************************\n" +
           "                  JUEGO CONECTA 4\n"
           "*****************************************************\n" +
@@ -19,52 +20,45 @@ def mostrarMenu():
           "********************************************************")
     global jugadores
     global jugadorinicial
-    p1=dict()
-    p1["nombrep1"]=[input("Jugador 1, indica tu nombre: ")]
-    letraP1 = input("Elige X o O:")
-    while not (letraP1 == 'X' or letraP1 == 'O' or letraP1 == 'x' or letraP1 == 'o'):
-        print('Letra incorrecta.Elige X or O?')
-        letraP1 = input("X o O")
-    if letraP1 == 'X' or letraP1 == 'x':
-        print("Has elegido X")
-        if letraP1 == "x":
-            letraP1 = "X"
-    elif letraP1 == 'O' or letraP1 == 'o':
-        print("Has elegido O")
-        if letraP1 == "o":
-            letraP1 = "O"
-    p1["letraP1"]=[letraP1]
-    jugadores.append(p1)
+    global p1
+    global p2
+    global letraP1
+    global letraP2
     
-    p2=dict()
-    p2["nombrep2"]=[input("Jugador 2,indica tu nombre: ")]
-
+    p1["nombre"]=input("Jugador 1, indica tu nombre: ")
+    letraP1 = input("Elige X o O:")
+    while not (letraP1=='X' or  letraP1=='O' or letraP1=='x' or letraP1=='o'):
+        letraP1=input('Letra incorrecta.Elige X or O?')
+    #Convertir letra a mayúscula e insertamos en la lista como diccionario
+    if letraP1 == "x":
+        letraP1 = "X"
+    elif letraP1 == "o":
+        letraP1 = "O"      
+    p1["letra"]=letraP1
+    jugadores.append(p1)
+    p2["nombre"]=input("Jugador 2,indica tu nombre: ")
     if letraP1 == "X":
         letraP2 = "O"
     elif letraP1 == "O":
         letraP2 = "X"
-
-    p2["letraP2"]=[letraP2]
+    p2["letra"]=letraP2
     jugadores.append(p2)
+    print(p2.get("nombre"),"te ha tocado el carácter:",p2.get("letra"))
     
-    print(p2.get("nombrep2"),"te ha tocado el carácter:",p2.get("letraP2"))
     random.seed(time.time())
-    jugadorinicial=jugadores[random.randint(0,1)]
+    jugadorinicial=random.randint(0,1)
 
-    #for para acceder al nombre del jugador que ha sido seleccionado
-    print("Comienza:",end="")
-    """tengo un error, me impime dos veces el jugador que va a empezar. hay que investigar"""
-    for i in (len(jugadores),[jugadorinicial]):
-        if jugadorinicial==0:
-            print(p1.get("nombrep1"))
-
-        else:
-            print(p2.get("nombrep2"))
+    print("Empieza ",end="")    
+    if jugadorinicial==0:
+        print(p1.get("nombre") + "!\n")
+    else:
+        print(p2.get("nombre") + "!\n")
 
     return jugadorinicial
 
 #este procedimiento imprime el tablero
-def mostrarTablero(jugador1):
+"""Isma creará una función que cree el tablero y lo devuelva"""
+def mostrarTablero():
     global lista_tablero
     for i in range(len(num_columnas)):
         if i==(len(num_columnas)-1):
@@ -77,27 +71,34 @@ def mostrarTablero(jugador1):
                    print(lista_tablero[i][j])
              else:
                    print(lista_tablero[i][j],end="")
-                   
-    insertarFicha(jugador1)
 
 #este procedimiento inserta las fichas de los jugadores
-def insertarFicha(jugador1):
-    #mientras se trate del jugador con valor O ejecuta esto:
-    columna=int(input("\nPor favor, JUGADOR1 dime la columna: "))
-    #aquí veririficamos que el usuario indique un número correcto de columna para colocar su ficha
-    while columna>7 or columna<1:
-        columna=int(input("la columna que has indicado no es correcta, dime otra: "))
+def insertarFicha(jugador):
+    if jugador == 0:
+        jugador=p1
+    else:
+        jugador=p2
+        
+    while victoria==False:
+        columna=int(input("\nPor favor " + jugador.get("nombre") + " dime la columna: "))
+        
+        while columna>7 or columna<1: #veririfición número correcto de columna
+            columna=int(input("la columna que has indicado no es correcta, dime otra: "))
 
-    #recorremos la columna indicada por el usuario para colocar su ficha en el hueco disponible
-    #for jugador que ha seleccionado la O
-    for i in range(len(lista_tablero)-1,-1,-1):
-        if lista_tablero[i][columna]=="_|":
-            lista_tablero[i][columna]="O|"
-            break
-        elif lista_tablero[0][1]!="_|":
-            columna=int(input("la columna que has indicado está completa, dime otra: "))
+        #recorremos la columna indicada por el usuario para colocar su ficha en el hueco disponible
+        for i in range(len(lista_tablero)-1,-1,-1):
+            if lista_tablero[i][columna]=="_|":
+                lista_tablero[i][columna]=jugador.get("letra") + "|"
+                break
+            elif lista_tablero[0][1]!="_|":
+                columna=int(input("la columna que has indicado está completa, dime otra: "))
 
-    mostrarTablero()
+        if jugador==p1:
+            jugador=p2
+        else:
+            jugador=p1
+            
+        mostrarTablero()
     #verificar victoria
 
 #VARIABLES
@@ -110,10 +111,15 @@ lista_tablero=[["|","_|","_|","_|","_|","_|","_|","_|"],\
                ["|","_|","_|","_|","_|","_|","_|","_|"],\
                ["|","_|","_|","_|","_|","_|","_|","_|"]]
 jugadores=[]
+p1=dict()
+p2=dict()
+letraP1=" "
+letraP2=" "
 victoria=False
 jugadorinicial=" "
 
 #Comienza el procedimiento
 mostrarMenu()
 while victoria==False:
-        mostrarTablero(jugadorinicial)
+        mostrarTablero()
+        insertarFicha(jugadorinicial)
