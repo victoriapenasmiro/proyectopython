aeaac479b04d7f59fe18421bb777d6c76fd278a6
victoriapenasmiro <mpenas@cifpfbmoll.eu>
2019-11-23 15:14:07 +0100

    la victoria vertical ya funcionagit add .!

--- comprobarvictoriavertical.py
+++ comprobarvictoriavertical.py
@@ -1,12 +1,11 @@
 def comprobarVictoriaVertical(i,jugador,columna):
     global victoria
-    print (i,jugador,columna)
     if i==0 or i==1 or i==2:
         if lista_tablero[i][columna]==(jugador.get("letra") + "|") and\
         lista_tablero[i+1][columna]==(jugador.get("letra") + "|") and\
         lista_tablero[i+2][columna]==(jugador.get("letra") + "|") and\
         lista_tablero[i+3][columna]==jugador.get("letra") + "|":
-            victoria==True
+            victoria=True
             print("\n",jugador.get("nombre"),"¡¡¡HAS GANADO!!!")
             
     return victoria
